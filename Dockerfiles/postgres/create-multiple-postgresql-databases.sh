215fc34c82baed2a5e41cc1891ea6d78ba7d84a8
Mark Sherman <mark.sherman@vk.team>
2022-11-20 20:42:46 +0300

    Fixed databases creation

--- Dockerfiles/postgres/create-multiple-postgresql-databases.sh
+++ Dockerfiles/postgres/create-multiple-postgresql-databases.sh
@@ -16,7 +16,7 @@ EOSQL
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
 	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
 	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
-		create_databases $db $POSTGRES_USER
+		create_database $db $POSTGRES_USER
 	done
 	echo "Multiple databases created"
 fi
